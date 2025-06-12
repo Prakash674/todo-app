95c2c7422bea327dcb978349a59f6a96db7ced0a
Prakash Kumar <singhprakash9572@gmail.com>
2025-06-12 08:52:01 +0530

    Add .gitignore and improve code formatting in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const { createTodo, updateTodo } = require('./types');
+app.use(express.json());
 
-app.use(express.json())
-
-//create todos  
+//create todos
 //body{title:
 // }
 app.post('/todo', (req, res) => {
- 
-})
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createTodo);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'Your sent the wrong inputs',
+    });
+    return;
+  }
+  //put it in mongodb
+});
 
+app.get('/todos', (req, res) => {});
 
-app.get('/todos',(req,res)=>{
-
-})
-
-app.put('/completed',(req,res)=>{
-  
-})
-
+app.put('/completed', (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updatePayload.safeParse(updateTodo);
+  if (!parsedPayload) {
+    res.status(411).json({
+      msg: 'You sent the Wrong Inputs',
+    });
+    return;
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
