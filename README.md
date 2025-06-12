eee16edf9662a7cc32ba2b2b712519c8031b70da
Prakash Kumar <singhprakash9572@gmail.com>
2025-06-12 08:41:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Todo app
+This Project contains a simple TODO application
+It has the following features-
+
+ - anyone can create a todo
+ - anyone can see their existing todos
+ - anyone can mark a todo as done
+
+
+
+ //intialize a node project 
+ //put a package.json
+ 
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+//create todos  
+//body{title:
+// }
+app.post('/todo', (req, res) => {
+ 
+})
+
+
+app.get('/todos',(req,res)=>{
+
+})
+
+app.put('/completed',(req,res)=>{
+  
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
